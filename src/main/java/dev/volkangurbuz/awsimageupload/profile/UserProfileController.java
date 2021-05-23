3ff89268d33d18ea19c198d17196ccf72d6ff741
volkangurbuz <volkangurbuz16@gmail.com>
2021-05-23 15:10:34 +0200

    added react

--- src/main/java/dev/volkangurbuz/awsimageupload/profile/UserProfileController.java
+++ src/main/java/dev/volkangurbuz/awsimageupload/profile/UserProfileController.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
+@CrossOrigin("*") // for only local
 public class UserProfileController {
 
   private final UserProfileService userProfileService;
